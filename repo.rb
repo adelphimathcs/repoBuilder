76ee85a57dc3e32bcc2c0badb816ffd369b5f3aa
Michael Vessia <michaelvessia@mail.adelphi.edu>
2015-07-30 11:46:43 -0400

    updated repo.rb to adhere to ruby style guide

--- repo.rb
+++ repo.rb
@@ -1,15 +1,15 @@
 require 'github_api'
 
 if ARGV.size != 3
-  puts "wrong amount of args"
+  puts 'wrong amount of args'
   Kernel.exit(true)
 else
   repo = ARGV[0]
   login = ARGV[1]
-  pw   =ARGV[2]
+  pw = ARGV[2]
 end
 
-file = File.open("keys.txt","r")
+file = File.open('keys.txt', 'r')
 
 client_id = file.readline.chomp
 client_secret = file.readline.chomp
@@ -23,30 +23,29 @@ github = Github.new client_id: client_id,
                     client_secret: client_secret
 res = github.authorize_url scope: 'repo'
 
-puts "please navigate your browser to: " + res.to_s
-puts "Enter your url code "
+puts 'please navigate your browser to: ' + res.to_s
+puts 'Enter your url code '
 
 auth_code = $stdin.gets.chomp
 token = github.get_token(auth_code)
 
 github = Github.new do |c|
   c.user          = login
-  c.basic_auth    = login+":"+pw
+  c.basic_auth    = login + ':' + pw
   c.repo          = repo
   c.oauth_token   = token.to_s
   c.client_id     = client_id
   c.client_secret = client_secret
 end
 
-#creates repo, throws error if already exists
+# creates repo, throws error if already exists
 github.repos.create name: repo, auto_init: true
 
-contents = Github::Client::Repos::Contents.new 
+contents = Github::Client::Repos::Contents.new
 file = contents.find path: 'README.md', user: login, repo: repo
 
-
 contents.update login, repo, 'README.md',
-  path: 'README.md',
-  message: "update readme",
-  content: "#readme content",
-  sha: file.sha
+                path: 'README.md',
+                message: 'update readme',
+                content: '#readme content',
+                sha: file.sha
