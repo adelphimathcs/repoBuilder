c4cbed9a5526dc54075191dc63f3fc80840d84f4
Matt Matero <matthewmatero@mail.adelphi.edu>
2015-08-02 03:52:19 -0400

    creates repos for alumni team

--- repo.rb
+++ repo.rb
@@ -12,11 +12,6 @@ file = File.open('keys.txt', 'r')
 client_id = file.readline.chomp
 client_secret = file.readline.chomp
 
-# github = Github.new basic_auth: login+':'+pw
-# github.oauth.app.create client_id: client_id,
-#                       client_secret: client_secret,
-#                       scopes: ['repo']
-
 github = Github.new client_id: client_id,
                     client_secret: client_secret
 res = github.authorize_url scope: 'repo'
@@ -39,9 +34,18 @@ end
 
 default_readme = File.open('default_readme.md', 'rb') { |f| f.read }
 
+#puts github.orgs.teams.list org: 'adelphimathcs'
+#response from team list
+#<Hashie::Mash description=nil id=1644006 members_url="https://api.github.com/teams/1644006/members{/member}" name="Faculty" permission="admin" privacy="closed" repositories_url="https://api.github.com/teams/1644006/repos" slug="faculty" url="https://api.github.com/teams/1644006">
+#<Hashie::Mash description="Current students at Adelphi " id=1646171 members_url="https://api.github.com/teams/1646171/members{/member}" name="Students" permission="admin" privacy="closed" repositories_url="https://api.github.com/teams/1646171/repos" slug="students" url="https://api.github.com/teams/1646171">
+#<Hashie::Mash description="Adelphi Alum" id=1646188 members_url="https://api.github.com/teams/1646188/members{/member}" name="Alumni" permission="admin" privacy="closed" repositories_url="https://api.github.com/teams/1646188/repos" slug="alumni" url="https://api.github.com/teams/1646188">
+
+
 # creates repo, throws error if already exists
 github.repos.create name: repo, org: github.org,
-                    description: 'Please put problem name here'
+                    description: 'Please put problem name here',
+                    team_id: 1646188 #1646171 <- student team ID 
+
 
 github.repos.contents.create github.user, repo,  'README.md',
                              org: github.org,
